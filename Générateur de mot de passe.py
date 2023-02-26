cc3955b7eb9bbf1366b352f284a893515811ebda
Dany Dubé <74633244+danydube1971@users.noreply.github.com>
2023-02-26 12:57:17 -0500

    Update Générateur de mot de passe.py

--- Générateur de mot de passe.py
+++ Générateur de mot de passe.py
@@ -12,7 +12,10 @@ def generate_password(keyword1, keyword2, keyword3):
     while len(password) > 16:
         password = password[:-1]
     while len(password) < 16:
-        password += random.choice(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '$', '#', '@', '&', '*', '+', '=', '^'])
+        password += random.choice(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 
+                                   'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 
+                                   'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '$', '#', '@', '&', 
+                                   '*', '+', '=', '^'])
 
     # Mélange des caractères pour plus de sécurité
     password_list = list(password)
